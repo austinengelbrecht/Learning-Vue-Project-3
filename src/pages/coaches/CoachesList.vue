679aaee7092d265536ab3361d5f02593d6528a19
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-04-11 20:08:38 -0500

    Checking for coaches

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -7,11 +7,13 @@
       <router-link to="/register">Register</router-link>
     </div>
 
-    <ul>
+    <ul v-if="hasCoaches">
       <li v-for="coach in filteredCoaches" :key="coach.id">
         {{ coach.firstName }}
       </li>
     </ul>
+
+    <h3 v-else>No Coaches Found</h3>
 
     List of coaches
   </section>
@@ -22,6 +24,9 @@ export default {
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
     },
   },
 };
