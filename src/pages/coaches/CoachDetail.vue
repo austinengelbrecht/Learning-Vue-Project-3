05459b833b7b490cce2841cf1ca1320fd245126e
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-04-17 21:42:56 -0500

    Add data handling

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -12,14 +12,52 @@
         <h2>Interested? Reach out now!</h2>
         <base-button link :to="contactLink">Contact</base-button>
       </header>
-      <router-view></router-view>
+      <!-- <router-view></router-view> -->
     </base-card>
   </section>
 
   <section>
     <base-card>
-      <base-badge></base-badge>
+      <base-badge
+        v-for="area in selectedCoach.areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      ></base-badge>
       <p>{{ description }}</p>
     </base-card>
   </section>
 </template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+  computed: {
+    fullName() {
+      return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
+    },
+    contactLink() {
+      return this.$route.path + '/' + this.id + '/contact';
+    },
+    areas() {
+      return this.selectedCoach.areas;
+    },
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+    description() {
+      return this.selectedCoach.description;
+    },
+  },
+  created() {
+    this.selectedCoach = this.$store.getters['coaches/coaches'].find(
+      (coach) => coach.id === this.id
+    );
+  },
+};
+</script>
