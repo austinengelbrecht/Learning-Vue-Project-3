d27cf8fff24e458dd5d0b160245956fc612ad34a
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-06-03 11:30:37 -0500

    Setup Contact form

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,1 +1,61 @@
-<template>Contact Coach C1</template>
+<template>
+  <form>
+    <div class="form-control">
+      <label for="email">Your Email</label>
+      <input type="email" name="email" id="email" />
+    </div>
+
+    <div class="form-control">
+      <label for="message">Your Message</label>
+      <textarea type="text" name="message" id="message" rows="5" />
+    </div>
+
+    <div class="actions">
+      <base-button>Send Message</base-button>
+    </div>
+  </form>
+</template>
+
+<style>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
