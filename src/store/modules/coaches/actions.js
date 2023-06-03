8fdbfdd5df36e6491020338d23d4fb4523f2d2dc
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-06-03 12:20:22 -0500

    Sending data to the server

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coach = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
@@ -9,6 +9,20 @@ export default {
       areas: data.areas,
     };
 
-    context.commit('registerCoach', coach);
+    const response = await fetch(
+      `https://test-back-end.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coach),
+      }
+    );
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error...
+    }
+
+    context.commit('registerCoach', { ...coach, id: userId });
   },
 };
