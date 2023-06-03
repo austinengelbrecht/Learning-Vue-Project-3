d3a9fecdd34c5b9308eaf8d7397eee7f472a2d45
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-06-03 11:35:21 -0500

    Setup basic validation

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,20 +1,50 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="email">Your Email</label>
-      <input type="email" name="email" id="email" />
+      <input type="email" name="email" id="email" v-model.trim="email" />
     </div>
 
     <div class="form-control">
       <label for="message">Your Message</label>
-      <textarea type="text" name="message" id="message" rows="5" />
+      <textarea
+        type="text"
+        name="message"
+        id="message"
+        rows="5"
+        v-model.trim="message"
+      />
     </div>
-
+    <p class="errors" v-if="!formIsValid">
+      Please enter a valid email and non empty mesage
+    </p>
     <div class="actions">
       <base-button>Send Message</base-button>
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      message: '',
+      formIsValid: true,
+    };
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+
+      if (this.email == '' || this.email.includes('@') || this.message == '') {
+        this.formIsValid = false;
+        return;
+      }
+    },
+  },
+};
+</script>
 
 <style>
 form {
