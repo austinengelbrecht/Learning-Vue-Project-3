db722dc5a8de817d83572fc83124e189c9a69786
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-06-04 19:36:05 -0500

    Add backend

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,27 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
       userEmail: payload.email,
       message: payload.message,
       coachId: payload.coachId,
-      id: new Date().toISOString(),
     };
+
+    const response = await fetch(
+      `https://test-back-end.com/coaches/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed');
+      throw error;
+    }
+
+    newRequest.id = responseData.name;
 
     context.commit('addRequest', newRequest);
   },
