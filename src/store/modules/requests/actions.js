61e341eee624d5089ae79a4b185ef832b5f702f4
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-06-04 19:47:24 -0500

    Setup getting data

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -22,7 +22,38 @@ export default {
     }
 
     newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
 
     context.commit('addRequest', newRequest);
+  },
+
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+
+    const response = await fetch(
+      `https://test-back-end.com/coaches/${coachId}.json`
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed');
+      throw error;
+    }
+
+    const requests = [];
+
+    for (const key in responseData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: responseData[key].userEmail,
+        message: responseData[key].message,
+      };
+
+      requests.push(request);
+    }
+
+    context.commit('');
   },
 };
