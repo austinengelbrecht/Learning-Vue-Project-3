33131177b211f3f0a09cacd67b67c2f390c36487
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-06-13 19:56:31 -0500

    Setup state for signup

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,1 +1,26 @@
-export default {};
+export default {
+  login() {},
+  async signup(context, payload) {
+    const response = await fetch('http://linktomybackend.com/signup', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to authenticate');
+      throw error;
+    }
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+};
